b904f369e8b4655bf517facb1168df6ed84c97a4
weiilinhan <298998024@qq.com>
2025-09-12 21:14:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 begin_weight=float(input())
-moon = 0.165
+moon_change = 0.165
 earth = 0.5
-years=10
-for i in range(1,years+1):
+for i in range(1,11):
+    earth_weight=begin_weight+earth*i
+    moon_weight=earth_weight*moon_change
+    print（f"第{i}年，地球体重为{earth_weight:.2f}kg,月球体重为{moon_weight:.2f}kg"）
+    
     
   
 
