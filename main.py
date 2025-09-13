4e30968a2b57651f2cf64f4a2aa5b6a6188b1cbd
weiilinhan <298998024@qq.com>
2025-09-13 16:31:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 begin_weight=float(input('请输入你当前在地球上的体重:'))
-for i in range(10):
+print('未来10年地球和月球体重变化：')
+for i in range(1,11):
     earth_weight=begin_weight+0.5*i
     moon_weight=earth_weight*0.165
     print（f'第{i}年，地球体重为{earth_weight:.2f}kg,月球体重为{moon_weight:.2f}kg'）
