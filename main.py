c0ae629306a0d9c745860a5688626e0e62c0d362
weiilinhan <298998024@qq.com>
2025-09-13 16:42:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ print('未来10年地球和月球体重变化:')
 for year in range(1,11):
     earth_weight = weight + a* year
 	moon_weight = earth_weight * 0.165	
-print(f'第{year}年:地球上的体重{earth_weight:.2f}kg,月球上的体重{moon_weight:.2f}kg'
+    print(f'第{year}年:地球上的体重{earth_weight:.2f}kg,月球上的体重{moon_weight:.2f}kg'
     
   
 
